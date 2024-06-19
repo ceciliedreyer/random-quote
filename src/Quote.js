b8b30486db76e165aae10f370b1433d4eb8fe3f9
ceciliedreyer <149535863+ceciliedreyer@users.noreply.github.com>
2024-06-19 10:21:27 +0200

    Imported font awesome and updated CSS

--- src/Quote.js
+++ src/Quote.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser, faFolder } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import "./Quote.css";
 
 export default function Quote() {
-  return <div>quote</div>;
+  return (
+    <div className="quote">
+      <h1>
+        <strong className="quote-icon">
+          <FontAwesomeIcon icon={faQuoteLeft} />
+        </strong>{" "}
+        <span className="quote-copy">this is the new quote</span>
+      </h1>
+    </div>
+  );
 }
--- src/QuoteGenerator.js
+++ src/QuoteGenerator.js
@@ -1,23 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUser, faFolder } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faTumblr } from "@fortawesome/free-brands-svg-icons";
 import "./QuoteGenerator.css";
 import Quote from "./Quote";
+library.add(faUser, faFolder);
 
 export default function QuoteGenerator() {
   return (
     <div className="quote-generator">
       <div className="wrapper">
-        <h1 className="quote">
+        <div className="quote">
           <Quote />
-        </h1>
+        </div>
         <p className="author">
           <em>- Cecilie Dreyer</em>
         </p>
         <div className="buttons">
-          <a className="social-share-button button">
-            <i class="bi bi-twitter"></i>
+          <a className="social-share-button button twitter">
+            <FontAwesomeIcon icon={faTwitter} />
           </a>
-          <button className="social-share-button button">F</button>
+          <button className="social-share-button button tumblr">
+            <FontAwesomeIcon icon={faTumblr} />
+          </button>
           <button className="new-quote-button button">new quote</button>
         </div>
       </div>
