546506884aed8b7339f52ef3390fb62bed76edfa
ceciliedreyer <149535863+ceciliedreyer@users.noreply.github.com>
2024-06-19 11:45:11 +0200

    Added quote API

--- src/Quote.js
+++ src/Quote.js
@@ -5,14 +5,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 import "./Quote.css";
 
-export default function Quote() {
+export default function Quote(props) {
   return (
     <div className="quote">
       <h1>
         <strong className="quote-icon">
           <FontAwesomeIcon icon={faQuoteLeft} />
         </strong>{" "}
-        <span className="quote-copy">this is the new quote</span>
+        <span className="quote-copy">{props.data.content}</span>
       </h1>
     </div>
   );
--- src/QuoteGenerator.js
+++ src/QuoteGenerator.js
@@ -10,15 +10,29 @@ import Quote from "./Quote";
 library.add(faUser, faFolder);
 
 export default function QuoteGenerator() {
+  const [quote, setQuote] = useState("hello there");
+
+  function handleSearch(response) {
+    setQuote(response.data);
+  }
+
+  function getQuote(event) {
+    event.preventDefault();
+    let apiUrl = "https://api.quotable.io/random";
+    axios.get(apiUrl).then(handleSearch);
+  }
+
   return (
     <div className="quote-generator">
       <div className="wrapper">
         <div className="quote">
-          <Quote />
+          <Quote data={quote} />
         </div>
-        <p className="author">
-          <em>- Cecilie Dreyer</em>
-        </p>
+        <div>
+          <p className="quote-author">
+            <em>- {quote.author}</em>
+          </p>
+        </div>
         <div className="buttons">
           <a className="social-share-button button twitter">
             <FontAwesomeIcon icon={faTwitter} />
@@ -26,7 +40,9 @@ export default function QuoteGenerator() {
           <button className="social-share-button button tumblr">
             <FontAwesomeIcon icon={faTumblr} />
           </button>
-          <button className="new-quote-button button">new quote</button>
+          <button className="new-quote-button button" onClick={getQuote}>
+            new quote
+          </button>
         </div>
       </div>
       <footer>
